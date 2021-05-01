6df98c9e88ddef879a0ab72c893e8b88fb9083f6
Ralph <rshipard@gmail.com>
2021-05-02 00:11:42 +1000

    html class and css updates

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# tictactoe
-Tic Tac Toe Game
+# Tic Tac Toe Game
 
-test text
+This is a game designed as my first project within the GA Software Immersive Flex Program.
+
+The game itself needs little introduction, the rules are included as part of the website. Instead I thought if anyone wanted to see my reasoning and why I've chosen certain methods this would be the best place to properly explain why I've chosen what I have.
+
+I will be comming back to this document and updating it over time, however please don't expect it to be kept updated exactly with any changes I have made. Indeed this may be a good snapshot of one way I tried to to this project.
--- main.js
+++ main.js
@@ -6,9 +6,14 @@ generateStartPlayerButton.addEventListener('click', function (event) {
   let startplayerDisplay = document.querySelector('#startPlayer')
   let startPlayerRandomiser = Math.floor(Math.random() * 2)
   let playerArray = [playerOneName, playerTwoName]
+  playerColors = []
   startPlayer = playerArray[startPlayerRandomiser]
-  startplayerDisplay.textContent = startPlayer
+  startplayerDisplay.textContent = startPlayer + " who will start by playing blue, click on a square to start, then take turns clicking on the squares."
 })
+
+const arrayColors = ['blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue']
+
+
 
 // previous value = "x"
 // now inside a funtion change previous value = "o" etc
